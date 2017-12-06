63f8cdbf8257a66307b9ab5579dbb3e8bbd91089
Robert Groves <rwgdev@gmail.com>
2017-12-05 21:38:14 -0600

    Initial commit of Unity project files.

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+    private int level;
+    private int round;
+    private const int maxRounds = 3;
+
+    // The current level's play surface.
+    private PushButtonSurface playSurface;
+
+    // TODO refactor this into an actual state enum
+    // Game States
+    private const int NewLevel = 0;
+    private const int ShowWinPattern = 1;
+    private const int NextRound = 2;
+    private const int EndGame = 99;
+
+    // The current game state.
+    private int curState;
+
+    // This array contains references to the surfaces that make up the play area. 
+    // These should be assigned from the editor using game objects from the scene that represent the surfaces that contain the push buttons which player will interact with.
+    // The push button objects should be child objects of the surface game object and tagged as "PushButton".
+    public GameObject[] pushButtonSurfaces;
+
+    // Use this for initialization
+    void Start () {
+        // Initialize the starting level and round.
+        level = 0;
+        round = 0;
+
+        curState = NewLevel;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        switch (curState)
+        {
+            case NewLevel:
+                // Increment to next level.
+                level++;
+
+                if ((level - 1) >= pushButtonSurfaces.Length)
+                {
+                    curState = EndGame;
+                }
+                else
+                {
+                    // Will be the first round of the new level.
+                    round = 1;
+
+                    // Set the current play surface for this level
+                    playSurface = pushButtonSurfaces[level - 1].GetComponent<PushButtonSurface>();
+
+                    // Set number of button pushes that should be in the win pattern for this level/round.
+                    int numberOfButtonPushesToWin = (2 * playSurface.PushButtonCount) + (round - 2);
+
+                    Debug.Log("***");
+                    Debug.Log("Level: " + level + "; Round: " + round + "; Pushes To Win: " + numberOfButtonPushesToWin);
+
+                    // Generate the pattern that corresponds to the correct order of 
+                    // button pushes the player must make to win.
+                    playSurface.GenerateWinPattern(numberOfButtonPushesToWin);
+                    curState = ShowWinPattern;
+                }
+
+                break;
+
+            case NextRound:
+                // Increment to next round.
+                round++;
+
+                if (round <= maxRounds)
+                {
+                    // TODO Refactor this is similar code as in NewLevel state
+
+                    // Set number of button pushes that should be in the win pattern for this level/round.
+                    int numberOfButtonPushesToWin = (2 * playSurface.PushButtonCount) + (round - 2);
+
+                    Debug.Log("===");
+                    Debug.Log("Level: " + level + "; Round: " + round + "; Pushes To Win: " + numberOfButtonPushesToWin);
+
+                    // Generate the pattern that corresponds to the correct order of 
+                    // button pushes the player must make to win.
+                    playSurface.GenerateWinPattern(numberOfButtonPushesToWin);
+                    curState = ShowWinPattern;
+                }
+                else
+                {
+                    curState = NewLevel;
+                }
+                break;
+
+            case ShowWinPattern:
+                Debug.Log("Showing the win pattern.");
+                playSurface.ShowWinPattern();
+
+                /* TODO the next state should be to wait for the players input and evaluate their button presses
+                        but for now just running through all levels/rounds to examine log output for correct play sequence. 
+                */
+                curState = NextRound;
+                break;
+
+            case EndGame:
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#endif
+                break;
+        }
+
+    }
+}
---
+++ Assets/Scripts/PushButtonSurface.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushButtonSurface : MonoBehaviour {
+    private const string pushButtonTag = "PushButton";
+
+    private List<GameObject> pushButtons = new List<GameObject>();
+    private List<int> winPattern = new List<int>();
+
+    public int PushButtonCount { get { return pushButtons.Count;  } }
+
+    // Use this for initialization
+    void Start () {
+        // Find all the push buttons on this surface and store them in the pushButtons list.
+        foreach (Transform child in transform)
+        {
+            if (child.tag == pushButtonTag)
+            {
+                pushButtons.Add(child.gameObject);
+            }
+        }
+
+        Debug.Log("pushButtons found: " + PushButtonCount);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GenerateWinPattern(int numberOfButtonActivations)
+    {
+        Debug.Log("Generating win pattern...");
+        // Clear any previous pattern stored.
+        winPattern.Clear();
+
+        // Generate the pattern of button pushes that will cause a win condition.
+        for (int i = 0; i < numberOfButtonActivations; i++)
+        {
+            int randomIdx = Random.Range(0, pushButtons.Count);
+            winPattern.Add(randomIdx);
+            Debug.Log("\tPicked: " + randomIdx);
+        }
+    }
+
+    public void ShowWinPattern()
+    {
+        // TODO this should light up the push buttons in the sequence stored in the win pattern. For now just logging details for simulation purposes.
+
+        Debug.Log("This is the win pattern:");
+        for (int i = 0; i < winPattern.Count; i++)
+        {
+            pushButtons[winPattern[i]].GetComponent<PushButton>().Glowing = true;
+            Debug.Log("\tPush " + (i + 1) + ": " + winPattern[i]);
+        }
+    }
+}
