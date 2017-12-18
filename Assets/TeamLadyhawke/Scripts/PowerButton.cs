a625861627f9dadb079aa2d601de39e0c1ca3cee
Robert Groves <rwgdev@gmail.com>
2017-12-17 23:46:26 -0600

    Added power button sound effect.

--- Assets/TeamLadyhawke/Scripts/PowerButton.cs
+++ Assets/TeamLadyhawke/Scripts/PowerButton.cs
@@ -6,6 +6,7 @@ public class PowerButton : MonoBehaviour, IPointerClickHandler
     // TOOD should remove gamestate and set up an Observer pattern for the click event
     public GameState gameState;
     private Animator animator;
+    private AudioSource audioSource;
 
     private void Awake()
     {
@@ -14,24 +15,33 @@ public class PowerButton : MonoBehaviour, IPointerClickHandler
         {
             Debug.LogError("Animator could not be found for " + gameObject.name);
         }
+
+        audioSource = gameObject.GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            Debug.LogError("AudioSource could not be found for " + gameObject.name);
+        }
     }
 
     public void StandBy()
     {
         // When the power button is in standby it's "glow" should be enabled.
         animator.SetBool("Activate", true);
+        audioSource.Play();
     }
 
     public void TurnOn()
     {
-        // When the power button is turned on it's "glow" should be disabled.
+        // When the power button is turned on it's "glow" and sound should be disabled.
         animator.SetBool("Activate", false);
+        audioSource.Stop();
     }
 
     public void TurnOff()
     {
-        // When the power button is turned off it's "glow" should be disabled.
+        // When the power button is turned off it's "glow" and sound should be disabled.
         animator.SetBool("Activate", false);
+        audioSource.Stop();
     }
 
     public void OnPointerClick(PointerEventData eventData)
