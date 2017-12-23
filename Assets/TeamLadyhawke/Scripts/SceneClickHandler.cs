7d8b6ae267c383c39d33e17dfbd361a4f7c28497
Robert Groves <rwgdev@gmail.com>
2017-12-22 20:40:07 -0600

    Added Play button for switch to main scene.

---
+++ Assets/TeamLadyhawke/Scripts/SceneClickHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SceneClickHandler : MonoBehaviour
+{
+    public void SceneClick()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+        switch (currentSceneName)
+        {
+            case "TitleScreen":
+                SceneManager.LoadScene("Main", LoadSceneMode.Single);
+                break;
+
+            case "GameOverCredits":
+                SceneManager.LoadScene("TitleScreen", LoadSceneMode.Single);
+                break;
+
+            default:
+                Debug.LogError("Unhandled scene name:" + currentSceneName);
+                break;
+        }
+    }
+
+}
